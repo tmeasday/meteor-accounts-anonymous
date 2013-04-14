36f94a7909b0a09e4eb6782bdc316ced96eb4ca4
rissem <rissem@gmail.com>
2013-04-14 14:27:28 -0700

    update to work with new meteor

--- anonymous_client.js
+++ anonymous_client.js
@@ -1,8 +1,6 @@
-(function() {
-  Meteor.loginAnonymously = function(fn) {
-    Meteor.call('login', {anonymous: true}, function(err, result) {
-      Meteor.accounts.makeClientLoggedIn(result.id, result.token);
-      fn && fn();
-    });
-  }
-})();
+Meteor.loginAnonymously = function(fn) {
+  Meteor.call('login', {anonymous: true}, function(err, result) {
+    Accounts._makeClientLoggedIn(result.id, result.token);
+    fn && fn();
+  });
+}
--- anonymous_server.js
+++ anonymous_server.js
@@ -1,17 +1,7 @@
-(function() {
-  
-  // handler to login anonymously
-  Meteor.accounts.registerLoginHandler(function(options) {
-    if (!options.anonymous)
-      return undefined; // don't handle
-    
-    // ok; if they are logging in, this means they don't have
-    // a user yet. Create one. We don't need to ever find it again.
-    var user = Meteor.accounts.onCreateUserHook({}, {}, {exists: true});
-    var userId = Meteor.users.insert(user);
-    
-    // create a login token (this is how we remember them)
-    var loginToken = Meteor.accounts._loginTokens.insert({userId: userId});
-    return {token: loginToken, id: userId};
-  });
-})();
+// handler to login anonymously
+Accounts.registerLoginHandler(function(options) {
+  if (!options.anonymous)
+    return undefined; // don't handle
+  _.extend(options, {generateLoginToken: true});
+  return Accounts.insertUserDoc(options, {});
+});
